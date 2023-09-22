82c79376c95ab219dadf083b6820a33febc0ead0
mschwann <marekschwann@gmail.com>
2023-09-22 20:18:16 +0100

    Added nvim setup

---
+++ roles/install_nvim/files/nvim/lua/custom/mappings.lua
@@ -0,0 +1,21 @@
+local M = {}
+M.general = {
+  n = {
+    ["<leader>q"] = {":%bd|e#<CR>", "Close all buffelrs, but current"},
+  }
+}
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+
+return M
